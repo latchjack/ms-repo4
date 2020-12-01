aa4e370cbbe13cee1a12b0a69c5b6399ea85b90d
Latch Jackn <latch.jack@gmail.com>
2020-12-01 14:36:45 +0000

    swapped identifiers from strings to actionTypes

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actions';
+
 const initialState = {
   counter: 0,
   results: []
@@ -5,32 +7,32 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'INCREMENT':
+    case actionTypes.INCREMENT:
         const newState = Object.assign({}, state); // Option1: immutably copy the state like this (new empty object on the left and the object you want to copy on the right)
         newState.counter = state.counter + 1; // add the change you want to the new object.
         return newState // return the new object in the switch statement.
-    case 'DECREMENT':
+    case actionTypes.DECREMENT:
       return {
         ...state, // Option2: without spreading here, we would lose the results array from our state, as it would set the new state with only the counter
         counter: state.counter - 1 // add the change you want to the new object.
       }
-    case 'ADD':
+    case actionTypes.ADD:
       return {
         ...state,
         counter: state.counter + action.value
       }
-    case 'SUBTRACT':
+    case actionTypes.SUBTRACT:
       return {
         ...state,
         counter: state.counter - action.value
       }
-    case 'STORE_RESULT':
+    case actionTypes.STORE_RESULT:
       return {
         ...state,
         results: state.results.concat({id: new Date(), value: state.counter}) 
         // concat is just like push(), except push adjusts the original array and concat creates a new array with the added value.
       }
-    case 'DELETE_RESULT':
+    case actionTypes.DELETE_RESULT:
       // const id = 2; // OPTION 1
       // const newArray = [...state.results];
       // newArray.splice(id, 1)
